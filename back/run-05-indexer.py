dc23f7bb1ed6fa4bad29951f8e2568d0f045746c
zhenwork <zhenwork@users.noreply.github.com>
2019-01-27 08:54:47 -0800

    s

--- back/run-05-indexer.py
+++ back/run-05-indexer.py
@@ -7,7 +7,7 @@ parser.add_argument("-run", "--run", help="run number", default=-1, type=int)
 parser.add_argument("-det", "--det", help="detector alias (e.g. DscCsPad)", default="DscCsPad", type=str) 
 parser.add_argument("-pkTag", "--pkTag", help="tag of cxi file", default="", type=str) 
 parser.add_argument("-noe", "--noe", help="number of images", default=-1, type=int)
-parser.add_argument("-distance", "--distance", help="distance + or - in mm", default=None, type=str)
+parser.add_argument("-distance", "--distance", help="distance + or - in mm", default=None, type=str, action="store_true")
 parser.add_argument("-pdb", "--pdb", help="pdb", default=None, type=str)
 args = parser.parse_args()
 
