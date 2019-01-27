f3927773dfcc715c890d1713dcf5e33eeb00cda7
zhenwork <zhenwork@users.noreply.github.com>
2019-01-27 06:07:05 -0800

    changes

--- back/run-04-peakFinder.py
+++ back/run-04-peakFinder.py
@@ -6,12 +6,14 @@ parser.add_argument("-exp", "--exp", help="experiment name", default="", type=st
 parser.add_argument("-run", "--run", help="run number", default=-1, type=int)
 parser.add_argument("-det", "--det", help="detector alias (e.g. DscCsPad)", default="DscCsPad", type=str) 
 parser.add_argument("-pkTag", "--pkTag", help="tag of cxi file", default="", type=str) 
+parser.add_argument("-noe", "--noe", help="number of images", default=-1, type=int)
 args = parser.parse_args()
 
 
-pf = pfHelper.peakFinderHelper(args.exp, args.run, args,det)
+pf = pfHelper.peakFinderHelper(args.exp, args.run, args.det)
 pf.setDefaultParams()
 pf.setAdaptiveMode()
 pf.pkTag = args.pkTag
+pf.noe = args.noe
 
-print pk.getCommand()
+print pf.getCommand()
