64460399c9ce0b7d36694db6b49ddc31ff21c4ff
zhenwork <zhenwork@users.noreply.github.com>
2019-01-27 10:16:05 -0800

    Update run-06-optimizeDistance.py

--- back/run-06-optimizeDistance.py
+++ back/run-06-optimizeDistance.py
@@ -1,8 +1,18 @@
+import os
+import subprocess
 import evaluateIndexing as ei 
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-stream", "--stream", help="stream file", default=None, type=str)
 args = parser.parse_args()
 
-score = ei.evaluateIndexing(args.stream)
-print score, np.sum(score[:3])
+process = subprocess.Popen("find "+args.stream, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+out, err = process.communicate()
+out = out.split('\n')
+
+for streamfile in out:
+	if not os.path.isfile(streamfile):
+		continue
+	score = ei.evaluateIndexing(streamfile)
+	print "streamfile: ", streamfile
+	print "score: ", np.sum(score[:3]), score
