72228b2e9efc990956025d92e130049335497144
zhenwork <zhenwork@users.noreply.github.com>
2019-01-27 11:31:07 -0800

    Update run-06-optimizeDistance.py

--- back/run-06-optimizeDistance.py
+++ back/run-06-optimizeDistance.py
@@ -16,7 +16,7 @@ for streamfile in out:
 	if not os.path.isfile(streamfile):
 		continue
 	print "##### streamfile: ", streamfile
-	print "##### with pdb? ", bool(args.pdb), args.pdb, type(args.pdb)
-	score = ei.evaluateIndexing(streamfile, withpdb=bool(args.pdb))
+	print "##### with pdb? ", bool(args.pdb.lower() in ["true", "yes", '1'])
+	score = ei.evaluateIndexing(streamfile, withpdb=bool(args.pdb.lower() in ["true", "yes", '1'])
 	print "##### score: ", np.sum(score[:3]), np.sum(score[3:]), np.sum(score), score,'\n\n'
 
